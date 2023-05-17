9761778fab8b24d4475f95a6305f44b9816671d0
manuv16 <manuella.v@escolar.ifrn.edu.br>
2023-05-17 17:06:15 -0300

    anotacoes prog web

---
+++ src/public/facebook-clone/a.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>php test</title>
+</head>
+<body>
+    <?php
+    $a = 3;
+    function teste()
+    {
+        $a += 2;
+    }
+    teste();
+    echo $a;
+    ?>
+</body>
+</html>
---
+++ src/public/facebook-clone/contador.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contador</title>
+</head>
+<body>
+    <?php
+    function contador()
+    {
+        static $total = 0;
+        return $total++;
+    }
+    for ($i=1;$i<=5;$i++){
+        echo contador();
+    }
+    ?>
+</body>
+</html>
